4da9dc026dee2f17bc1b8ecf7fb560eebda31d0f
maxflex <makcyxa-k@yandex.ru>
2020-07-08 02:53:33 +0300

    Report limit decreased

--- back/app/Models/Report.php
+++ back/app/Models/Report.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Report extends Model
 {
     // допустимое кол-во жалоб. далее исключаем задачу
-    const LIMIT = 5;
+    const LIMIT = 3;
 
     protected $fillable = ['task_id'];
 
